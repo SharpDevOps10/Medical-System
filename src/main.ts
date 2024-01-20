2a269668df349b4bde3a6364f6dfb4100c948639
SharpDevOps10 <td090833@gmail.com>
2024-01-20 16:55:23 +0200

    configuration #8: added database connection + configs
    - added docker-compose.yml
    - added swagger page
    - added module for TypeORM
    - updated dependencies
    
    closes #8

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap () {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.APP_PORT;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    })
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Media Request API')
+    .setDescription('Here is Media Request API documentation')
+    .setVersion('2.0.4')
+    .addTag('api')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port);
+  console.info(`Started server on 127.0.0.1:${port}`);
 }
 bootstrap();
